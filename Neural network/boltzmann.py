15d08489e0ba5baadf570d5e2f8a63ea8065f23b
radosnystudent <jeremiasz.pauszek@gmail.com>
2020-07-10 22:00:17 +0200

    more updates, fix MB

--- Neural network/boltzmann.py
+++ Neural network/boltzmann.py
@@ -1,82 +1,106 @@
-from math import pow, log
-import math
-from random import random, randint, getrandbits
+from math import pow, log, exp
+from random import random, randint
 
-temperature: float = 1.0
+
+'''
+Im mniejsza jest temperatura tym bardziej maszyna Boltzmanna dąży do sieci Hopfielda,
+a dla wysokich temperatur maszyna Boltzmana jest procesem Markowa.
+
+T - temperatura
+1) dla T >> 0 maszyna Boltzmana działa losowo
+2) dla niewielkich T Maszyna Boltzmana działa według sieci Hopfielda:
+1.0 , gdy u(t) > 0
+0.0 , gdy u(t) < 0
+'''
 
 
 def generateCVector(z: list) -> list:
     """
-    Function generate vector based on given input vector
-    @param: (list of floats) - list contains 0.0 and 1.0, size = 20
-    @return: (list of floats) - generated vector (list)
+    wektor początkowy
     """
 
-    return [[0.0 if i != j else (z[i] - 0.5)*(z[j] - 0.5) for j in range(20)] for i in range(20)]
+    return [
+        [0.0 if i == j else (z[i] - 0.5) * (z[j] - 0.5) for j in range(20)]
+        for i in range(20)
+    ]
 
 
-def calculateNextVector(prevVector: list, cVector: list) -> list:
+def calculateTheta(cVector: list) -> list:
     """
-    Boltzman
-    @param: (list of floats) - previous vector from this function (first can be randomly generated); size = 20
-    @param: (list of floats) - cVecotr (generated in function generateCVector); size = 20
-    @return: (list of floats) - calculated vector (list); size = 20
+    obliczanie parametru theta
+    """
+    return [sum(row) for row in cVector]
+
+
+def calculateW(cVector: list) -> list:
+    """
+    liczenie macierzy wag
+    """
+    return [[2 * value for value in row] for row in cVector]
+
+
+def calculateUvalue(
+    prevVector: list, row: int, wVector: list, thetaVector: list
+) -> float:
+    """
+    wartosc u - suma (wektor wag razy stan) - wartosc theta
+    """
+    return sum([wVector[row][j] * prevVector[j] for j in range(20)]) - thetaVector[row]
+
+
+def calculateF(u: float, temperature: float) -> float:
+    """
+    wartosc F dla argumentów u i temperatury
+    """
+    return (lambda u: 1.0 / (1.0 + exp(-u / temperature)))(u)
+
+
+def calculateNextVector(
+    prevVector: list, wVector: list, thetaVector: list, temperature: float
+) -> list:
+    """
+    generowanie nastepnego stanu maszyny boltzmana
     """
 
     result: list = list()
-    f_value: any = None
 
     for i in range(20):
-
-        u_value = 0.0
-
-        # obliczanie wag, potrzebnych do wyliczenia wartości u
-        for j in range(20):
-            w = 2*cVector[i][j]
-            u_value += w * prevVector[j]
-        # odjęcie wartości progowej, która jest sumą wartości wektora C[i]
-        u_value -= sum(cVector[i])
-
-        # obliczenie wartości funkcji f dla argumentu u
-        f_value = (lambda u: 1.0 /
-                   (1.0 + math.exp(u / temperature)))(u_value)
-
         # jeśli temperatura jest większa od 1.0, MB działa losowo,
         # w przypadku gdy jest mniejsza od 1.0 działa według sieci Hopfielda
 
-        if temperature >= 1.0:
-            if f_value > randint(0, 10) * 1.0:
-                result.append(1.0)
-            else:
-                result.append(0.0)
+        if (
+            calculateF(
+                calculateUvalue(prevVector, i, wVector, thetaVector), temperature
+            ) >= random()
+        ):
+            result.append(1.0)
         else:
-            if u_value > 0.0:
-                result.append(1.0)
-            elif u_value < 0.0:
-                result.append(0.0)
+            result.append(0.0)
+
     return result
 
 
 def decodeResult(vector: list) -> str:
     """
-    Function coding 0.0 to  and 1.0 to ■
-    @param: (list) - vector of 0.0 and 1.0
-    @return: (str) - coded vector of numbers
+    kodowanie wektora do znaków: 0.0 ->  i 1.0 -> ■
     """
 
-    return ' '.join([u"\u25A1" if value == 0.0 else u"\u25A0" for value in vector])
+    return " ".join(["\u25A1" if value != 1.0 else "\u25A0" for value in vector])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    temperature: float = 0.1
 
     z: list = [0.0 if i < 10 else 1.0 for i in range(20)]
 
-    cVector = generateCVector(z)
+    cVector: list = generateCVector(z)
     xVector: list = [randint(0, 1) * 1.0 for _ in range(20)]
-    print(f'początkowy: {decodeResult(xVector)}\n')
+
+    wVector: list = calculateW(cVector)
+    thetaVector: list = calculateTheta(cVector)
+    print(f"początkowy: {decodeResult(xVector)}\n")
 
     for _ in range(10):
-        temperature = randint(1, 50) / 10
-        xVector = calculateNextVector(xVector, cVector)
+        xVector: list = calculateNextVector(xVector, wVector, thetaVector, temperature)
         print(decodeResult(xVector))
-        print(f'temperatura: {temperature}\n')
--- Neural network/gradient.py
+++ Neural network/gradient.py
@@ -1,6 +1,6 @@
 from math import pow
 
-# stała c i eps
+# stała c i epsilon
 c_const: float = 0.01
 eps_const: float = 0.0000001
 
@@ -25,21 +25,25 @@ def gradient(inputVec: list, functionNumber: int) -> str:
     while True:
         localVec = inputVec[:]  # tworzenie kopii wektora
 
-        if functionNumber == 1: # obliczanie wartosci jesli badamy funkcje 1
+        if functionNumber == 1:  # obliczanie wartosci jesli badamy funkcje 1
             # stała c przemnożona przez pochodna dla x1 funkcji pierwszej
-            newVector[0] = inputVec[0] - c_const * \
-                (lambda vector: 4*vector[0] - 2*vector[1] - 2)(inputVec)
+            newVector[0] = inputVec[0] - c_const * (
+                lambda vector: 4 * vector[0] - 2 * vector[1] - 2
+            )(inputVec)
             # stała c przemnożona przez pochodna dla x2 funkcji pierwszej
-            newVector[1] = inputVec[1] - c_const * \
-                (lambda vector: 2*vector[1] - 2*vector[0])(inputVec)
+            newVector[1] = inputVec[1] - c_const * (
+                lambda vector: 2 * vector[1] - 2 * vector[0]
+            )(inputVec)
 
-        elif functionNumber == 2: # obliczanie wartosci jesli badamy funkcje 2        
+        elif functionNumber == 2:  # obliczanie wartosci jesli badamy funkcje 2
             # stała c przemnożona przez pochodna dla x1 funkcji drugiej
-            newVector[0] = inputVec[0] - c_const * (lambda vector: 2*pow(
-                vector[0], 3) - pow(vector[0], 2) - vector[0])(inputVec)
+            newVector[0] = inputVec[0] - c_const * (
+                lambda vector: 2 * pow(vector[0], 3) - pow(vector[0], 2) - vector[0]
+            )(inputVec)
             # stała c przemnożona przez pochodna dla x2 funkcji drugiej
-            newVector[1] = inputVec[1] - c_const * \
-                (lambda vector: 2*vector[1] - 2)(inputVec)
+            newVector[1] = inputVec[1] - c_const * (lambda vector: 2 * vector[1] - 2)(
+                inputVec
+            )
 
         inputVec = localVec
 
@@ -49,17 +53,25 @@ def gradient(inputVec: list, functionNumber: int) -> str:
             if abs(value - inputVec[i]) > maxVart:
                 maxVart = abs(value - inputVec[i])
 
-        # sprawdzenie warunku stopu, jeśli spełniony to wypisujemy wynik w zależności która funkcje sprawdzalismy
+        # sprawdzenie warunku stopu, jeśli spełniony to wypisujemy wynik
+        # w zależności która funkcje sprawdzalismy
         if maxVart < eps_const:
             if functionNumber == 1:
-                return f'Dla parametrów: x1 = {newVector[0]}, x2 = {newVector[1]}, funkcja F1 ma wartość: {round(2*pow(newVector[0],2) + pow(newVector[1],2) - 2*newVector[0]*newVector[1] - 2*newVector[1] + 1,4)}'
+                return (
+                    f"Dla parametrów: x1 = {newVector[0]}, x2 = {newVector[1]},"
+                    + f"funkcja F1 ma wartość: {round(2*pow(newVector[0],2) + pow(newVector[1],2) - 2*newVector[0]*newVector[1] - 2*newVector[1] + 1,4)}"
+                )
             elif functionNumber == 2:
-                return f'Dla parametrów: x1 = {newVector[0]}, x2 = {newVector[1]}, funkcja F2 ma wartość: {round(pow(newVector[0],4)/2 - pow(newVector[0],3)/3 - pow(newVector[0],2)/2 + pow(newVector[1],2) - 2*newVector[1] + 1, 4)}'
+                return (
+                    f"Dla parametrów: x1 = {newVector[0]}, x2 = {newVector[1]},"
+                    + f"funkcja F2 ma wartość: {round(pow(newVector[0],4)/2 - pow(newVector[0],3)/3 - pow(newVector[0],2)/2 + pow(newVector[1],2) - 2*newVector[1] + 1, 4)}"
+                )
 
         inputVec = newVector
 
 
-# print(gradient([ , ], 1)) # minimum lokalne funkcji 1 ... ?
-print(gradient([1.0, 1.0], 1))  # minimum globalne funkcji 1
-print(gradient([-0.5, 1.0], 2))  # minimum lokalne funkcji 2
-print(gradient([1.0, 1.0], 2))  # minimum globalne funkcji 2
+if __name__ == "__main__":
+    # print(gradient([ , ], 1)) # minimum lokalne funkcji 1 ... ?
+    print(gradient([1.0, 1.0], 1))  # minimum globalne funkcji 1
+    print(gradient([-0.5, 1.0], 2))  # minimum lokalne funkcji 2
+    print(gradient([1.0, 1.0], 2))  # minimum globalne funkcji 2
